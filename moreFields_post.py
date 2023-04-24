5a076fafb28ffe84512d55d91987b869950aaee3
yoshi8777 <123114539+yoshi8777@users.noreply.github.com>
2023-04-24 09:44:31 +0530

    Update moreFields_post.py

--- moreFields_post.py
+++ moreFields_post.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
  all_fields = ','.join(all_fields)
  posts = graph.get_connections('me', 'posts', fields=all_fields)
  while True: # keep paginating
- try:
- with open('my_posts.jsonl', 'a') as f:
- for post in posts['data']:
- f.write(json.dumps(post)+"\n")
- # get next page
- posts = requests.get(posts['paging']['next']).json()
- except KeyError:
- # no more pages, break the loop
- break
+    try:
+        with open('my_posts.jsonl', 'a') as f:
+            for post in posts['data']:
+                f.write(json.dumps(post)+"\n")
+                # get next page
+            posts = requests.get(posts['paging']['next']).json()
+    except KeyError:
+    # no more pages, break the loop
+        break
