72ad655d52993c74734f93ebf99172eb1b78db64
yoshi8777 <123114539+yoshi8777@users.noreply.github.com>
2023-06-25 20:29:32 +0530

    Add files via upload

---
+++ twitter/1. TwitterClient.py
@@ -0,0 +1,40 @@
+# Chap02/twitter_client.py
+#import os
+import sys
+from tweepy import API
+from tweepy import OAuthHandler
+
+def getTokenFromFile(fileName):
+    with open(fileName, encoding='utf-8') as fp:
+        return fp.readline()
+
+def get_twitter_auth():
+    """Setup Twitter authentication.
+
+    Return: tweepy.OAuthHandler object
+    """
+    try:
+        consumer_key = getTokenFromFile("APIKey.txt")
+        consumer_secret = getTokenFromFile("APIKeySecret.txt")
+        access_token = getTokenFromFile("AccessToken.txt")
+        access_secret = getTokenFromFile("AccessTokenSecret.txt")
+
+    except KeyError:
+        sys.stderr.write("TWITTER_* environment variables not set\n")
+        sys.exit(1)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    return auth
+
+def get_twitter_client():
+    """Setup Twitter API client.
+
+    Return: tweepy.API object
+    """
+    auth = get_twitter_auth()
+    client = API(auth)
+    print("Auth: ", auth)
+    print("Client: ", client)
+    return client
+
+get_twitter_client()
---
+++ twitter/2. TwGetHomeTimeline.py
@@ -0,0 +1,14 @@
+# Chap02/twitter_get_home_timeline.py
+import json
+from tweepy import Cursor
+from TwitterClient import get_twitter_client
+
+if __name__ == '__main__':
+
+    client = get_twitter_client()
+
+    with open('home_timeline.jsonl', 'w') as f:
+        for page in Cursor(client.home_timeline, count = 200).pages(4):
+            for status in page:
+                # Process a single status
+                f.write(json.dumps(status._json)+"\n")
