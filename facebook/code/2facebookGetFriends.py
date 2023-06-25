5d2a1f59b0ec59062fc0ae8367cdefb0de73ab3a
yoshi8777 <123114539+yoshi8777@users.noreply.github.com>
2023-06-26 04:57:22 +0530

    Add files via upload

---
+++ facebook/code/2facebookGetFriends.py
@@ -0,0 +1,21 @@
+# Chap04/facebook_get_friends.py
+import os
+import facebook
+import json
+
+
+def getTokenFromFile(fileName):
+    with open(fileName, encoding='utf-8') as fp:
+        return fp.readline()
+
+
+if __name__ == '__main__':
+    # token = os.environ.get('FACEBOOK_TEMP_TOKEN')
+    token = getTokenFromFile("FacebookToken.txt")
+    graph = facebook.GraphAPI(token)
+    user = graph.get_object("me")
+    friends = graph.get_connections(user["id"], "friends")
+    # list = [friend['name'] for friend in friends['data']]
+    print(json.dumps(friends, indent=4))
+
+# python 2facebookGetFriends.py
